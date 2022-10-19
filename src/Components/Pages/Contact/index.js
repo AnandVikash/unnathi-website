786dd0460106a8b7b410dbf3a39497b96c0c3fb9
prasanth <prashanth.digikit@gmail.com>
2022-10-19 18:16:48 +0530

    contact animation

--- src/Components/Pages/Contact/index.js
+++ src/Components/Pages/Contact/index.js
@@ -25,25 +25,20 @@ export default function index() {
       />
       <div className={styles.ContactMainContainer}>
         <div className={styles.ContactContainer}>
-          <div
-            className={styles.AddressContainer}
-            data-aos="fade-up-right"
-            data-aos-delay="300"
-            data-aos-duration="500"
-          >
-            <div className={styles.AddressItemsContainers}>
+          <div className={styles.AddressContainer}>
+            <div
+              className={styles.AddressItemsContainers}
+              data-aos="fade-up-right"
+              data-aos-delay="300"
+              data-aos-duration="500"
+            >
               <div className={styles.AddressItemContainer}>
                 <div className={styles.AddressItemIcon}>
                   <div className={styles.AddressIcon}>
                     <IoLocationSharp />
                   </div>
                 </div>
-                <div
-                  className={styles.AddressItemContent}
-                  data-aos="fade-up-right"
-                  data-aos-delay="300"
-                  data-aos-duration="500"
-                >
+                <div className={styles.AddressItemContent}>
                   <h3>Address</h3>
                   <p>
                     150, 2nd Floor, Above Vishwas Jewellers, Sanjay Nagar Main
@@ -57,12 +52,7 @@ export default function index() {
                     <FaEnvelope />
                   </div>
                 </div>
-                <div
-                  className={styles.AddressItemContent}
-                  data-aos="fade-up-right"
-                  data-aos-delay="300"
-                  data-aos-duration="500"
-                >
+                <div className={styles.AddressItemContent}>
                   <h3>Email</h3>
                   <p>healing.unnathi@gmail.com</p>
                 </div>
@@ -73,12 +63,7 @@ export default function index() {
                     <BsTelephoneFill />
                   </div>
                 </div>
-                <div
-                  className={styles.AddressItemContent}
-                  data-aos="fade-up-right"
-                  data-aos-delay="300"
-                  data-aos-duration="500"
-                >
+                <div className={styles.AddressItemContent}>
                   <h3>Phone</h3>
                   <p>+91 9845426049</p>
                 </div>
@@ -125,7 +110,12 @@ export default function index() {
           </div>
         </div>
       </div>
-      <div className={styles.contactMap}>
+      <div
+        className={styles.contactMap}
+        data-aos="fade-up-left"
+        data-aos-delay="300"
+        data-aos-duration="500"
+      >
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.0487342224183!2d77.57447261482264!3d13.032568590815954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae17e8059c75e1%3A0x20518939a8fddd6d!2sUnnathi%20Healing%20Foundation!5e0!3m2!1sen!2sin!4v1664782765574!5m2!1sen!2sin"
           width="100%"
