fe90272172aa4ea3f6ff37e3f90d956a385dccf0
raghu <anand@digikit.in>
2022-11-23 10:54:07 +0530

    asoociates

--- src/Components/Assets/ElementsUi/Associates/Index.js
+++ src/Components/Assets/ElementsUi/Associates/Index.js
@@ -2,17 +2,31 @@ import React from "react";
 import Slider from "react-slick";
 import SecTitle from "../SecTitle";
 import styles from "./associates.module.css";
+import associate1 from "../../../Assets/Associates/associate-1.png";
+import associate2 from "../../../Assets/Associates/associate-2.png";
+import associate3 from "../../../Assets/Associates/associate-3.png";
+import associate4 from "../../../Assets/Associates/associate-4.png";
+import associate5 from "../../../Assets/Associates/associate-5.png";
+import associate6 from "../../../Assets/Associates/associate-6.png";
+
 export default function Index() {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     speed: 500,
     autoplaySpeed: 2000,
     cssEase: "linear",
     responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
       {
         breakpoint: 576,
         settings: {
@@ -37,38 +51,39 @@ export default function Index() {
           <Slider {...settings}>
             <div>
               <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
+                <img src={associate1} />
+                <p>Department of Primary and Secondary Education, Karnataka</p>
+              </div>
+            </div>
+
+            <div>
+              <div className={styles.AssociateImg}>
+                <img src={associate2} />
+                <p>Bangalore North University (BNU), Kolar</p>
               </div>
             </div>
             <div>
               <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
+                <img src={associate3} />
+                <p>Government Ayurvedic Medical College, Bengaluru</p>
               </div>
             </div>
             <div>
               <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
+                <img src={associate4} />
+                <p>National Service Scheme, Karnataka</p>
               </div>
             </div>
             <div>
               <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
+                <img src={associate5} />
+                <p>GarbhaGudi IVF Center, Bengaluru </p>
               </div>
             </div>
             <div>
               <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
-              </div>
-            </div>
-            <div>
-              <div className={styles.AssociateImg}>
-                <img src="https://andit.co/projects/html/jago-welfare/assets/img/partner/logo-3.png" />
-                <p>lorem ipsum</p>
+                <img src={associate6} />
+                <p>DVS Group of Institutions, Shimoga </p>
               </div>
             </div>
           </Slider>
--- src/Components/Pages/About/Index.js
+++ src/Components/Pages/About/Index.js
@@ -18,7 +18,7 @@ export default function Index() {
       <Welcome />
       <OurTeam />
       <Associates />
-      <OurProjects />
+      {/* <OurProjects /> */}
       {/* <OurPartners /> */}
     </>
   );
